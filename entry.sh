1e33956aa3baaaf774f39fe0fc97e56af66434a8
Andrew Cutler <macropin@gmail.com>
2017-01-10 15:54:38 +1100

    Initial add of feature to add runtime users and MOTD setting feature. Refs #4

--- entry.sh
+++ entry.sh
@@ -24,10 +24,34 @@ if [ -w ~/.ssh/authorized_keys ]; then
     chown root:root ~/.ssh/authorized_keys
     chmod 600 ~/.ssh/authorized_keys
 fi
+if [ -w /etc/ssh/authorized_keys ]; then
+    chown root:root /etc/ssh/authorized_keys
+    chmod 755 /etc/ssh/authorized_keys
+    find /etc/ssh/authorized_keys/ -type f -exec chmod 644 {} \;
+fi
 
-# Warn if no config
-if [ ! -e ~/.ssh/authorized_keys ]; then
-  echo "WARNING: No SSH authorized_keys found for root"
+# Add users if SSH_USERS=user/uid/gid set
+if [ -v SSH_USERS ]; then
+    USERS=$(echo $SSH_USERS | tr "," "\n")
+    for USER in $USERS; do
+        IFS='/' read -ra UA <<< "$USER"
+        NAME=${UA[0]}
+        echo ">> Adding user $NAME with uid: ${UA[1]} gid: ${UA[2]}."
+        if [ ! -e " /etc/ssh/authorized_keys/$NAME" ]; then
+            echo "WARNING: No SSH authorized_keys found for $NAME!"
+        fi
+        adduser -D -u ${UA[1]} -g ${UA[2]} $NAME
+    done
+else
+    # Warn if no authorized_keys
+    if [ ! -e ~/.ssh/authorized_keys ] && [ ! $(ls -A /etc/ssh/authorized_keys) ]; then
+      echo "WARNING: No SSH authorized_keys found!"
+    fi
+fi
+
+# Update MOTD
+if [ -v MOTD ]; then
+    echo -e "$MOTD" > /etc/motd
 fi
 
 stop() {
