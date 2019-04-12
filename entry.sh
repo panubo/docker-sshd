dce45e8888b97dc1feb07899008889c6975620e9
Andrew Cutler <andrew@panubo.com>
2019-04-12 10:06:20 +1000

    Fix: Allow unlocking root and SSH_USERS

--- entry.sh
+++ entry.sh
@@ -83,11 +83,13 @@ else
     if [ ! -e ~/.ssh/authorized_keys ] && [ ! $(ls -A /etc/authorized_keys) ]; then
       echo "WARNING: No SSH authorized_keys found!"
     fi
-    if [[ "${SSH_ENABLE_ROOT}" == "true" ]]; then
-      usermod -p '' root  # Unlock root account
-    else
-      echo "WARNING: root account is now locked by default. Set SSH_USERS to unlock the account."
-    fi
+fi
+
+# Unlock root account, if enabled
+if [[ "${SSH_ENABLE_ROOT}" == "true" ]]; then
+    usermod -p '' root
+else
+    echo "WARNING: root account is now locked by default. Set SSH_USERS to unlock the account."
 fi
 
 # Update MOTD
